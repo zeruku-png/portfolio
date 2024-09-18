0b66b698a49ee84f5b005593d335149f208da958
zeruku <zrcblue@gmail.com>
2024-09-18 14:11:10 +0900

    Nothing

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const img = document.querySelector(".sep-img");
+    // 全ての画像を取得
+    const images = document.querySelectorAll('.sep-img');
 
+    // オプション設定
     const options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.4,
+        root: null,  // ビューポートを基準にする
+        rootMargin: '0px',  // 余白なし
+        threshold: 0.1  // 画像が10%表示されたら実行
     };
 
+    // 画像が表示領域に入った時の処理
     const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach((entry) => {
+        entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.style.transition = "filter 1s ease";
+                // 画像のぼかしを解除
+                entry.target.style.transition = "filter 1.3s ease";
                 entry.target.style.filter = "blur(0)";
+                // ぼかしが解除された後、監視を停止
                 observer.unobserve(entry.target);
             }
         });
     }, options);
 
-    observer.observe(img);
+    // すべての画像に対して監視を開始
+    images.forEach(image => {
+        observer.observe(image);
+    });
+});
+
+
+//scroll_effect
+$(window).scroll(function () {
+    var scrollAnimationElm = document.querySelectorAll('.scroll-up');
+    var scrollAnimationFunc = function () {
+        for (var i = 0; i < scrollAnimationElm.length; i++) {
+            var triggerMargin = 100;
+            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+                scrollAnimationElm[i].classList.add('on');
+            }
+        }
+    }
+    window.addEventListener('load', scrollAnimationFunc);
+    window.addEventListener('scroll', scrollAnimationFunc);
 });
