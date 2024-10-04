873f39dbaa9a404d62eaa65ea69b993d874f1ec5
zeruku <zrcblue@gmail.com>
2024-10-04 21:46:16 +0900

    一新。

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,36 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // 全ての画像を取得
-    const images = document.querySelectorAll('.sep-img');
+const header = document.querySelector(".header");
+let lastScrollY = window.scrollY;
+let isVisible = true;
+let hideTimeout;
 
-    // オプション設定
-    const options = {
-        root: null,  // ビューポートを基準にする
-        rootMargin: '0px',  // 余白なし
-        threshold: 0.1  // 画像が10%表示されたら実行
-    };
+function handleScroll() {
+    if (window.scrollY > lastScrollY) {
+        // スクロールダウン
+        if (isVisible) {
+            clearTimeout(hideTimeout);
+            header.style.transition =
+                "transform 0.2s ease-in-out, opacity 0.2s ease-in-out";
+            header.style.transform = "translateY(-10px)";
+            header.style.opacity = "0";
 
-    // 画像が表示領域に入った時の処理
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // 画像のぼかしを解除
-                entry.target.style.transition = "filter 1.3s ease";
-                entry.target.style.filter = "blur(0)";
-                // ぼかしが解除された後、監視を停止
-                observer.unobserve(entry.target);
-            }
-        });
-    }, options);
+            hideTimeout = setTimeout(() => {
+                header.style.transform = "translateY(-100%)";
+            }, 300);
 
-    // すべての画像に対して監視を開始
-    images.forEach(image => {
-        observer.observe(image);
-    });
-});
-
-
-//scroll_effect
-$(window).scroll(function () {
-    var scrollAnimationElm = document.querySelectorAll('.scroll-up');
-    var scrollAnimationFunc = function () {
-        for (var i = 0; i < scrollAnimationElm.length; i++) {
-            var triggerMargin = 100;
-            if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-                scrollAnimationElm[i].classList.add('on');
-            }
+            isVisible = false;
+        }
+    } else {
+        // スクロールアップ
+        if (!isVisible) {
+            clearTimeout(hideTimeout);
+            header.style.transition =
+                "transform 0.2s ease-in-out, opacity 0.2s ease-in-out";
+            header.style.transform = "translateY(0)";
+            header.style.opacity = "0.8";
+            isVisible = true;
         }
     }
-    window.addEventListener('load', scrollAnimationFunc);
-    window.addEventListener('scroll', scrollAnimationFunc);
-});
+    lastScrollY = window.scrollY;
+}
+
+window.addEventListener("scroll", handleScroll);
