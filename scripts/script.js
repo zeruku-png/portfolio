e4e127bec62e02bf01eba99bdc7b2c25d1b750c7
zeruku <zrcblue@gmail.com>
2024-11-14 14:25:06 +0900

    no message

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,130 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        $(".fv-title").css("opacity", function () {
-            if (0 > 1 - $(window).scrollTop() / ($(window).height() / 3)) {
-                $(".fv-title").addClass("show");
-                return;
-            } else {
-                $(".fv-title").removeClass("show");
-            }
-            return 1 - $(window).scrollTop() / ($(window).height() / 3);
-        });
-    });
-});
-
-
-window.onload = function () {
-    const spinner = document.getElementById("loading");
-    spinner.classList.add("loaded");
+// Constants
+const SCROLL_CONFIG = {
+    fadeOutRatio: 1 / 3,
+    backgroundParallaxRatio: 1 / 5,
 };
 
-$(function () {
-    $("header").each(function () {
-        var $window = $(window),
-            $header = $(this),
-            headerBoxTop = $header.offset().top;
-        galleryTop = $(".gallery").offset().top;
+// DOM Elements Cache
+const DOM = {
+    init() {
+        this.header = $("header");
+        this.fvTitle = $(".fv-title");
+        this.fadeStatic = $(".fd-stc");
+        this.navItems = {
+            profile: $(".item-profile"),
+            gallery: $(".item-gallery"),
+            skill: $(".item-skill"),
+        };
+        this.sections = {
+            gallery: $(".gallery"),
+            skill: $(".skill"),
+        };
+        this.spinner = $("#loading");
+    },
+};
 
-        $window.on("scroll", function () {
-            if ($window.scrollTop() > headerBoxTop) {
-                $header.addClass("sticky");
-                $(".fd-stc").addClass("show");
-            } else {
-                $header.removeClass("sticky");
-                $(".fd-stc").removeClass("show");
+// Scroll Effect Manager
+class ScrollEffectManager {
+    constructor() {
+        this.previousIndex = -1;
+        this.headerBoxTop = 0;
+        this.sectionTops = {
+            gallery: 0,
+            skill: 0,
+        };
+    }
+
+    init() {
+        DOM.init();
+        this.headerBoxTop = DOM.header.offset().top;
+        this.sectionTops.gallery = DOM.sections.gallery.offset().top;
+        this.sectionTops.skill = DOM.sections.skill.offset().top;
+        this.bindEvents();
+    }
+
+    bindEvents() {
+        $(window).on("scroll", () => this.handleScroll());
+        window.onload = () => this.handlePageLoad();
+    }
+
+    handlePageLoad() {
+        DOM.spinner.addClass("loaded");
+    }
+
+    handleScroll() {
+        const scrollTop = $(window).scrollTop();
+        this.updateFvTitleOpacity(scrollTop);
+        this.updateHeaderSticky(scrollTop);
+        this.updateNavigationState(scrollTop);
+        this.updateBackgroundPosition(scrollTop);
+    }
+
+    updateFvTitleOpacity(scrollTop) {
+        const opacity =
+            1 - scrollTop / ($(window).height() * SCROLL_CONFIG.fadeOutRatio);
+
+        if (opacity > 1) {
+            DOM.fvTitle.addClass("show");
+        } else {
+            DOM.fvTitle.removeClass("show");
+            DOM.fvTitle.css("opacity", Math.max(0, opacity));
+        }
+    }
+
+    updateHeaderSticky(scrollTop) {
+        if (scrollTop > this.headerBoxTop) {
+            DOM.header.addClass("sticky");
+            DOM.fadeStatic.addClass("show");
+        } else {
+            DOM.header.removeClass("sticky");
+            DOM.fadeStatic.removeClass("show");
+        }
+    }
+
+    updateNavigationState(scrollTop) {
+        let activeIndex = 0;
+
+        if (scrollTop >= this.sectionTops.skill - 100) {
+            activeIndex = 2;
+        } else if (scrollTop >= this.sectionTops.gallery - 100) {
+            activeIndex = 1;
+        }
+
+        if (activeIndex !== this.previousIndex) {
+            // Reset all navigation items
+            Object.values(DOM.navItems).forEach((item) =>
+                item.removeClass("big")
+            );
+
+            // Set active navigation item
+            switch (activeIndex) {
+                case 0:
+                    DOM.navItems.profile.addClass("big");
+                    break;
+                case 1:
+                    DOM.navItems.gallery.addClass("big");
+                    break;
+                case 2:
+                    DOM.navItems.skill.addClass("big");
+                    break;
             }
 
-            if ($window.scrollTop() > galleryTop) {
-                $(".item-profile").removeClass("big");
-                $(".item-gallery").addClass("big");
-            } else {
-                $(".item-profile").addClass("big");
-                $(".item-gallery").removeClass("big");
-            }
+            this.previousIndex = activeIndex;
+        }
+    }
 
-            $("body").css("background-position-y", function () {
-                if ($window.scrollTop() < headerBoxTop) {
-                    return -$(window).scrollTop() / 5;
-                } else {
-                    return;
-                }
-            });
-        });
-        $window.trigger("scroll");
-    });
+    updateBackgroundPosition(scrollTop) {
+        if (scrollTop < this.headerBoxTop) {
+            $("body").css(
+                "background-position-y",
+                -scrollTop * SCROLL_CONFIG.backgroundParallaxRatio
+            );
+        }
+    }
+}
+
+// Initialize on document ready
+$(document).ready(() => {
+    const scrollManager = new ScrollEffectManager();
+    scrollManager.init();
 });
